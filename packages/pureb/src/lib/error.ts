8bba30dcc65428c2d062ff87f65fc24d57a7ca53
blan19 <oponize@naver.com>
2024-03-23 17:54:51 +0900

    feat: add errorMessage enum

---
+++ packages/pureb/src/lib/error.ts
@@ -0,0 +1,9 @@
+enum ErrorMessage {
+  isNotHTMLElementMsg = 'Target is instance of HTMLElement',
+  isNotPropertyMsg = 'Property is not exist',
+  isAlreadyRegisteredRefMsg = 'Ref is already initialized.',
+  isEmptyStringMsg = 'String is empty',
+  isApiError = '',
+}
+
+export default ErrorMessage;
